eb4d3022bc0b028ef03ef97b4d29651fa3013f02
unknown <cesyrub@gmail.com>
2022-05-24 17:42:33 +0200

    Finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const [contactsList, setContactsList] = useState(contacts.slice(0, 5))
+
+  const addContact = () => {
+    if (contactsList.length === contacts.length) return;
+    const randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+    contactsList.includes(randomContact) ? addContact() : setContactsList([randomContact, ...contactsList]);
+  }
+
+  const sortContacts = (sortingBy) => {
+    console.log("sorting");
+    const contactsListCopy = JSON.parse(JSON.stringify(contactsList));
+    if (sortingBy === "name") {
+      setContactsList(contactsListCopy.sort((a, b) => a.name > b.name ? 1 : -1));
+    } else {
+      setContactsList(contactsListCopy.sort((a, b) => a.popularity < b.popularity ? 1 : -1));
+    }
+  }
+
+  const removeContact = (removeId) => {
+    setContactsList(contactsList.filter(elem => elem.id !== removeId))
+  }
+
+  return (<div className="App">
+    <h1>IRON CONTACTS</h1>
+    <div className="buttons">
+      <button className='btn greenBtn' onClick={addContact}>ADD RANDOM CONTACT</button>
+      <button className='btn greenBtn' onClick={() => sortContacts("name")}>SORT CONTACTS BY NAME</button>
+      <button className='btn greenBtn' onClick={() => sortContacts("popularity")}>SORT CONTACTS BY POPULARITY</button>
     </div>
-  );
-}
 
-export default App;
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Oscar</th>
+          <th>Emmy</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          contactsList.map((elem) => {
+            return (
+              <tr key={elem.id}>
+                <td className='pictureContact'><img src={elem.pictureUrl} alt="" height="300px" className='pictureContact' /></td>
+                <td>{elem.name}</td>
+                <td>{elem.popularity}</td>
+                <td>{elem.wonOscar ? <img src="oscar.png" alt="" height="150px" /> : ""}</td>
+                <td>{elem.wonEmmy ? <img src="emmy.png" alt="" height="150px" /> : ""}</td>
+                <td><button className='btn redBtn' onClick={() => removeContact(elem.id)}>REMOVE</button></td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  </div>);
+}
---
+++ src/components/Table.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Table(props) {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>The table body</td>
+                    <td>with two columns</td>
+                    <td>with two columns</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
